0fe3dda8513c86e078af1af42dcc29fe98d84b21
astarta16 <ngedeshuri2001@gmail.com>
2023-10-09 11:35:19 +0400

    add validation using yup

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,13 +1,23 @@
 import {} from "./Card";
-import {useForm} from 'react-hook-form'
+import { useForm } from "react-hook-form";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
 
 function Card() {
-    const {register, handleSubmit} = useForm();
+  const schema = yup.object().shape({
+    firstName: yup.string().required(),
+    userNAme: yup.string().required(),
+    email: yup.string().email().required(),
+    password: yup.string().min(2).max(100).required(),
+  });
 
-    const onSubmit = (data) => {
-        console.log(data)
-    };
+  const { register, handleSubmit } = useForm({
+    resolver: yupResolver(schema),
+  });
 
+  const onSubmit = (data) => {
+    console.log(data);
+  };
 
   return (
     <>
@@ -27,10 +37,22 @@ function Card() {
           </p>
         </div>
         <form onSubmit={handleSubmit(onSubmit)}>
-          <input type="text" placeholder="First name"  {...register("firstName")}/>
-          <input type="text" placeholder="User name"  {...register("userNAme")} />
-          <input type="text" placeholder="Email"  {...register("email")} />
-          <input type="password" placeholder="Password"  {...register("password")} />
+          <input
+            type="text"
+            placeholder="First name"
+            {...register("firstName")}
+          />
+          <input
+            type="text"
+            placeholder="User name"
+            {...register("userNAme")}
+          />
+          <input type="text" placeholder="Email" {...register("email")} />
+          <input
+            type="password"
+            placeholder="Password"
+            {...register("password")}
+          />
           <input type="submit" />
         </form>
       </div>
