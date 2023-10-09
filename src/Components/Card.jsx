2740ffe56a3e7850e1112a44bb129ea2e73f6c1e
astarta16 <ngedeshuri2001@gmail.com>
2023-10-09 12:09:17 +0400

    add basic styles usng styled components

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,7 +1,17 @@
-import {} from "./Card";
+import React from "react";
 import { useForm } from "react-hook-form";
 import * as yup from "yup";
 import { yupResolver } from "@hookform/resolvers/yup";
+import {
+    Container,
+    ContentContainer,
+    TextContainer,
+    FormContainer,
+    Title,
+    Text,
+    Form,
+  } from "./CardStyle";
+
 
 function Card() {
   const schema = yup.object().shape({
@@ -11,7 +21,7 @@ function Card() {
     password: yup.string().min(2).max(100).required("Password cannot be empty"),
   });
 
-  const { register, handleSubmit, formState: {errors} } = useForm({
+  const { register, handleSubmit, formState: { errors } } = useForm({
     resolver: yupResolver(schema),
   });
 
@@ -20,47 +30,34 @@ function Card() {
   };
 
   return (
-    <>
-      <div>
-        <h1>Learn to code by watching others</h1>
-        <p>
-          See how experienced developers solve problems in real-time. Watching
-          scripted tutorials is great, but understanding how developers think is
-          invaluable.{" "}
-        </p>
-      </div>
-      <div>
-        <div>
+    <Container>
+      <ContentContainer>
+        <TextContainer>
+          <Title>Learn to code by watching others</Title>
+          <Text>
+            See how experienced developers solve problems in real-time. Watching
+            scripted tutorials is great, but understanding how developers think is
+            invaluable.
+          </Text>
+        </TextContainer>
+        <FormContainer>
           <p>
-            {" "}
             Try it free 7 days <span>then $20/mo. thereafter</span>
           </p>
-        </div>
-        <form onSubmit={handleSubmit(onSubmit)}>
-          <input
-            type="text"
-            placeholder="First name"
-            {...register("firstName")}
-          />
-           <p>{errors.firstName?.message}</p> 
-          <input
-            type="text"
-            placeholder="User name"
-            {...register("userNAme")}
-          />
-             <p>{errors.userNAme?.message}</p> 
-          <input type="text" placeholder="Email" {...register("email")} />
-          <p>{errors.email?.message}</p> 
-          <input
-            type="password"
-            placeholder="Password"
-            {...register("password")}
-          />
-           <p>{errors.password?.message}</p> 
-          <input type="submit" />
-        </form>
-      </div>
-    </>
+          <Form onSubmit={handleSubmit(onSubmit)}>
+            <input type="text" placeholder="First name" {...register("firstName")} />
+            <p>{errors.firstName?.message}</p>
+            <input type="text" placeholder="User name" {...register("userNAme")} />
+            <p>{errors.userNAme?.message}</p>
+            <input type="text" placeholder="Email" {...register("email")} />
+            <p>{errors.email?.message}</p>
+            <input type="password" placeholder="Password" {...register("password")} />
+            <p>{errors.password?.message}</p>
+            <input type="submit" value="Submit" />
+          </Form>
+        </FormContainer>
+      </ContentContainer>
+    </Container>
   );
 }
 
--- src/Components/CardStyle.js
+++ src/Components/CardStyle.js
@@ -1,2 +1,72 @@
 import styled from "styled-components";
 
+export const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
+  background-color: #FF7979;
+`;
+
+export const ContentContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  @media (min-width: 768px) {
+    flex-direction: row;
+  }
+`;
+
+export const TextContainer = styled.div`
+  text-align: center;
+  color: white;
+  padding: 20px;
+
+  @media (min-width: 768px) {
+    text-align: left;
+  }
+`;
+
+export const FormContainer = styled.div`
+  background-color: white;
+  border-radius: 10px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+  text-align: center;
+  padding: 20px;
+  max-width: 400px;
+  width: 90%;
+
+  @media (min-width: 768px) {
+    text-align: left;
+    margin-left: 20px;
+  }
+`;
+
+export const Title = styled.h1`
+  font-size: 24px;
+  margin-bottom: 20px;
+`;
+
+export const Text = styled.p`
+  font-size: 16px;
+  margin-bottom: 20px;
+`;
+
+export const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+
+  input {
+    margin-bottom: 10px;
+    padding: 10px;
+    border: 1px solid #ccc;
+    border-radius: 5px;
+  }
+
+  p {
+    color: red;
+    text-align: left;
+    margin-bottom: 10px;
+  }
+`;
