e3ec2c2ca5354aaad67e6c48ddfdb3bb9932e664
astarta16 <ngedeshuri2001@gmail.com>
2023-10-09 11:05:34 +0400

    add inputs and install hook-form

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,10 +1,40 @@
-import {} from './Card'
+import {} from "./Card";
+import {useForm} from 'react-hook-form'
 
-function Card () {
-    return(
-        <div></div>
-        
-    );
+function Card() {
+    const {register, handleSubmit} = useForm();
+    const onSubmit = () => {
+        console.log("hey")
+    };
+
+
+  return (
+    <>
+      <div>
+        <h1>Learn to code by watching others</h1>
+        <p>
+          See how experienced developers solve problems in real-time. Watching
+          scripted tutorials is great, but understanding how developers think is
+          invaluable.{" "}
+        </p>
+      </div>
+      <div>
+        <div>
+          <p>
+            {" "}
+            Try it free 7 days <span>then $20/mo. thereafter</span>
+          </p>
+        </div>
+        <form onSubmit={handleSubmit}>
+          <input type="text" placeholder="name" />
+          <input type="text" placeholder="surname" />
+          <input type="text" placeholder="email" />
+          <input type="password" placeholder="password" />
+          <input type="submit" />
+        </form>
+      </div>
+    </>
+  );
 }
 
-export default Card
+export default Card;
