bb967ba1fc200e7c151f5dff837c2930831102aa
astarta16 <ngedeshuri2001@gmail.com>
2023-10-09 11:52:05 +0400

    add validation messages

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -5,13 +5,13 @@ import { yupResolver } from "@hookform/resolvers/yup";
 
 function Card() {
   const schema = yup.object().shape({
-    firstName: yup.string().required(),
-    userNAme: yup.string().required(),
-    email: yup.string().email().required(),
-    password: yup.string().min(2).max(100).required(),
+    firstName: yup.string().required("First Name cannot be empty"),
+    userNAme: yup.string().required("User Name cannot be empty"),
+    email: yup.string().email().required("Looks like this is not an email"),
+    password: yup.string().min(2).max(100).required("Password cannot be empty"),
   });
 
-  const { register, handleSubmit } = useForm({
+  const { register, handleSubmit, formState: {errors} } = useForm({
     resolver: yupResolver(schema),
   });
 
@@ -42,17 +42,21 @@ function Card() {
             placeholder="First name"
             {...register("firstName")}
           />
+           <p>{errors.firstName?.message}</p> 
           <input
             type="text"
             placeholder="User name"
             {...register("userNAme")}
           />
+             <p>{errors.userNAme?.message}</p> 
           <input type="text" placeholder="Email" {...register("email")} />
+          <p>{errors.email?.message}</p> 
           <input
             type="password"
             placeholder="Password"
             {...register("password")}
           />
+           <p>{errors.password?.message}</p> 
           <input type="submit" />
         </form>
       </div>
